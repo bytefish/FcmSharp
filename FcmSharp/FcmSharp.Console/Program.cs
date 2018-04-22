c8c91eeb708ec35f1ac33c7bc8046dcbb6d69951
Philipp Wagner <bytefish@gmx.de>
2018-04-22 11:53:38 +0200

    Update README, Increase Version Number to 2.0.0

--- FcmSharp/FcmSharp.Console/Program.cs
+++ FcmSharp/FcmSharp.Console/Program.cs
@@ -2,10 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using FcmSharp.Model.Options;
-using FcmSharp.Model.Topics;
 using FcmSharp.Requests;
 using FcmSharp.Settings;
 
