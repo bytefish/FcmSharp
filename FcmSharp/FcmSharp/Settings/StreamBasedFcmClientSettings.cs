00d90adcdc7984320b33f373a2000638c4e6a2cd
Philipp Wagner <pwag86@googlemail.com>
2018-09-15 10:22:19 +0200

    Issue #18 Adding User @janniksam to Copyright

--- FcmSharp/FcmSharp/Settings/StreamBasedFcmClientSettings.cs
+++ FcmSharp/FcmSharp/Settings/StreamBasedFcmClientSettings.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Philipp Wagner. All rights reserved.
+﻿// Copyright (c) Philipp Wagner and janniksam (https://github.com/janniksam). All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
