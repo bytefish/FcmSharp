295860610c9dfbc82515aeef4395d89f792e16b6
Philipp Wagner <pwag86@googlemail.com>
2019-04-07 14:48:05 +0200

    Issue #59 Adding Sample to Console App

--- FcmSharp/FcmSharp.Console/Program.cs
+++ FcmSharp/FcmSharp.Console/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using FcmSharp.Requests;
 using FcmSharp.Settings;
@@ -34,7 +35,7 @@ namespace FcmSharp.ConsoleApp
                         Notification = notification
                     }
                 };
-                
+
                 // Finally send the Message and wait for the Result:
                 CancellationTokenSource cts = new CancellationTokenSource();
 
@@ -43,6 +44,60 @@ namespace FcmSharp.ConsoleApp
 
                 // Print the Result to the Console:
                 Console.WriteLine("Data Message ID = {0}", result.Name);
+
+                Console.WriteLine("Press Enter to exit ...");
+                Console.ReadLine();
+            }
+
+            SendBatchMessages();
+        }
+
+        public static void SendBatchMessages()
+        {
+            // Read the Credentials from a File, which is not under Version Control:
+            var settings = FileBasedFcmClientSettings.CreateFromFile(@"D:\serviceAccountKey.json");
+
+            // Construct the Client:
+            using (var client = new FcmClient(settings))
+            {
+                const string token1 = "";
+                const string token2 = "";
+
+                var notification = new Notification
+                {
+                    Title = "Notification Title",
+                    Body = "Notification Body Text"
+                };
+
+                var messages = new[]
+                {
+                    new Message
+                    {
+                        Token = token1,
+                        Notification = notification
+                    },
+                    new Message
+                    {
+                        Token = token2,
+                        Notification = notification
+                    },
+                };
+
+                // Finally send the Message and wait for the Result:
+                CancellationTokenSource cts = new CancellationTokenSource();
+
+                // Send the Message and wait synchronously:
+                var result = client.SendBatchAsync(messages, false, cts.Token).GetAwaiter().GetResult();
+
+                // Print the Result to the Console:
+                Console.WriteLine($"Batch SuccessCount = {result.SuccessCount}, FailureCount = {result.FailureCount}");
+
+                Console.WriteLine("Batch Responses:");
+
+                foreach (var response in result.Responses)
+                {
+                    Console.WriteLine($"SendResponse MessageId = {response.MessageId}, Success = {response.Success}");
+                }
 
                 Console.WriteLine("Press Enter to exit ...");
                 Console.ReadLine();
